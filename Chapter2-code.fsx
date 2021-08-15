ec8ae461d84783d9dc5bc9d5636c6d454bda7793
Garrett van Wageningen <ghvanw283@gmail.com>
2021-08-14 19:49:10 -0500

    save

--- Chapter2-code.fsx
+++ Chapter2-code.fsx
@@ -1,27 +1,45 @@
 type Vector2D = double * double
 
+
 let vectorScalarOps (op : double -> double -> double) (n : double) (s : Vector2D) : Vector2D =
     match s with
     | (x, y) -> (op n x, op n y)
+
 
 let vectorOps (op : double -> double -> double) (f : Vector2D) (s : Vector2D) : Vector2D = 
     match (f, s) with
     | ((x, y), (x', y')) -> (op x x', op y y')
 
+
 let (|+|) = vectorOps (+)
 
+
 let (-*-) = vectorScalarOps (*)
+
 
 let addVecs ns = 
     ns |> Seq.fold (|+|) (0.0, 0.0)
 
+
+let addRevised (vectors: seq<Vector2D>) =
+    (vectors |> Seq.map (fun (x, _) -> x) |> Seq.sum, vectors |> Seq.map (fun (_, y) -> y) |> Seq.sum)
+
+
 let translate translation vecs =
     vecs |> Seq.map (fun v -> v |+| translation)
+
 
 let vectorsToScatterLists vecs =
     vecs |> Seq.fold (fun (xs, ys) (x, y) -> (x::xs, y::ys)) ([], [])
 
+
+let distance (v : Vector2D) : double =
+    let (x, y) = v
+    Math.Sqrt ((Math.Pow (x, 2.0)) + (Math.Pow (y, 2.0)))
+
+
 let (|-|) = vectorOps (-)
+
 
 let xys : seq<Vector2D> = 
     seq { 
@@ -29,12 +47,28 @@ let xys : seq<Vector2D> =
         for y in -5.0 .. 5.0 ->
         (x, y) }
 
+
 let thing : Vector2D -> seq<Vector2D> -> seq<Vector2D> = ((-*-) 10.0) >> translate 
+
 
 let other : seq<seq<Vector2D> -> seq<Vector2D>> = Seq.map thing xys
 
+
 let findSide otherSide hypotenuse = 
     Math.Sqrt (Math.Pow (hypotenuse, 2.0) - Math.Pow (otherSide, 2.0))
+
+
+let distanceBetween first second = distance (first |-| second)
+
+
+let perimeter vecs = 
+    vecs
+    |> Seq.windowed 2
+    |> Seq.map (fun window -> 
+        match window with
+        | [| x; y |] -> distanceBetween x y
+        | _ -> 0.0)
+    |> Seq.sum
 
 // ---------------------- XPlot Helpers ----------------------
 // let vecArrow (originx: double, originy: double) color (x: double, y: double) =
